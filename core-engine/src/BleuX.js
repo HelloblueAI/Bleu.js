5119e2c7984765732a24e812f48bbba19f68fcce
Pejman H. Nia <81389644+pejmantheory@users.noreply.github.com>
2024-06-04 21:12:56 -0700

    Push changes for troubleshooting

---
+++ core-engine/src/BleuX.js
@@ -0,0 +1,39 @@
+class Store {
+  constructor(options) {
+      this.state = new Proxy(options.state, {
+          set: (target, key, value) => {
+              target[key] = value;
+              return true;
+          }
+      });
+      this.mutations = options.mutations;
+      this.actions = options.actions;
+  }
+
+  commit(mutation, payload) {
+      this.mutations[mutation](this.state, payload);
+  }
+
+  dispatch(action, payload) {
+      return this.actions[action]({
+          commit: this.commit.bind(this),
+          state: this.state
+      }, payload);
+  }
+}
+
+const BleuX = {
+  install(Bleu) {
+      Bleu.prototype.$store = null;
+      Bleu.mixin({
+          beforeCreate() {
+              if (this.$options.store) {
+                  Bleu.prototype.$store = this.$options.store;
+              }
+          }
+      });
+  }
+};
+
+export { Store };
+export default BleuX;
---
+++ ui/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-transform-runtime'
+    ],
+    parserOpts: {
+      requireConfigFile: false
+    }
+  };
+  
