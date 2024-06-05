efe76cccc80d34d7e1088b7caadc76e128657c1a
Pejman H. Nia <81389644+pejmantheory@users.noreply.github.com>
2024-06-05 10:27:59 -0700

    Update Server.svg

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: '/',
   configureWebpack: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
-    }
-  }
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
 };
