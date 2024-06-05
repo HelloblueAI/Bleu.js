38abde3ae98e04033bb67bb49920e33c4f8e17cd
Pejman H. Nia <81389644+pejmantheory@users.noreply.github.com>
2024-06-05 00:45:00 -0700

    Fix Dockerfile paths and update docker-compose configuration

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,20 +1,24 @@
 import Bleu from '../../../core-engine/src/Bleu.js';
-import BleuX from '../../../core-engine/src/BleuX.js';
+import { Store } from '../../../core-engine/src/BleuX.js';
 
-Bleu.use(BleuX);
-
-export default new BleuX.Store({
-  state: {
-    message: 'Welcome to Bleu.js'
-  },
-  mutations: {
-    updateMessage(state, newMessage) {
-      state.message = newMessage;
-    }
-  },
-  actions: {
-    updateMessage({ commit }, newMessage) {
-      commit('updateMessage', newMessage);
-    }
+Bleu.use({
+  install(Bleu) {
+    Bleu.prototype.$store = new Store({
+      state: {
+        message: 'Welcome to Bleu.js'
+      },
+      mutations: {
+        updateMessage(state, newMessage) {
+          state.message = newMessage;
+        }
+      },
+      actions: {
+        updateMessage({ commit }, newMessage) {
+          commit('updateMessage', newMessage);
+        }
+      }
+    });
   }
 });
+
+export default Bleu.prototype.$store;
