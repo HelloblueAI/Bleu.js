a07b9b049c617960611243e7ebb3068cfac7c4a7
Pejman H. Nia <81389644+pejmantheory@users.noreply.github.com>
2024-06-07 21:08:42 -0700

    Update API and its documentation

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
 const port = 5000;
-=======
-const port = 5000;  
->>>>>>> 5bf653fe8871e41c3e51029ea561ce5388cf6f25
 
 app.use(express.json());
 
 app.post('/debug', (req, res) => {
-  // Debug logic
+
   res.send('Debugging');
 });
 
 app.post('/optimize', (req, res) => {
-  // Optimize logic
+
   res.send('Optimizing');
 });
 
 app.post('/generate', (req, res) => {
-  // Generate logic
+
   res.send('Generating');
 });
 
