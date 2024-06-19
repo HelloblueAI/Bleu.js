f57f3ec6b2155531f6617b3efb190c642f7d2aa3
Pejman Haghighatnia <pejmanhnia@gmail.com>
2024-06-18 18:01:50 -0700

    Optimize lint step to use cache and update CI/CD pipeline

--- babel.config.cjs
+++ babel.config.cjs
@@ -2,9 +2,11 @@ module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
+    '@babel/preset-react',  
   ],
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-runtime',
+    '@babel/plugin-syntax-jsx', 
   ],
 };
