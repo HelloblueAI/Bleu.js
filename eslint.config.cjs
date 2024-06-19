33934229073846c400f0fa111e1a6266d0737da7
Pejman Haghighatnia <pejmanhnia@gmail.com>
2024-06-18 18:48:57 -0700

    Update ESLint config for new format

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -3,46 +3,51 @@ const compat = new FlatCompat();
 
 module.exports = [
   {
-    files: ["**/*.js", "**/*.cjs"],
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.cjs'],
     languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
+      parser: require('@babel/eslint-parser'),
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
     },
     rules: {
-      "no-console": "warn",
-      "no-debugger": "warn",
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"],
-      "indent": ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      "eqeqeq": ["error", "always"],
-      "no-trailing-spaces": "error",
-      "space-before-blocks": "error",
-      "keyword-spacing": ["error", { "before": true, "after": true }],
-      "space-infix-ops": "error",
-      "comma-spacing": ["error", { "before": false, "after": true }],
-      "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
-      "space-unary-ops": ["error", { "words": true, "nonwords": false }]
-    }
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      eqeqeq: ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'space-before-blocks': 'error',
+      'keyword-spacing': ['error', { before: true, after: true }],
+      'space-infix-ops': 'error',
+      'comma-spacing': ['error', { before: false, after: true }],
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      'space-unary-ops': ['error', { words: true, nonwords: false }],
+    },
   },
   {
-    files: ["**/*.vue"],
+    files: ['**/*.vue'],
     plugins: {
-      vue: require("eslint-plugin-vue")
+      vue: require('eslint-plugin-vue'),
     },
     languageOptions: {
-      parser: require("vue-eslint-parser"),
+      parser: require('vue-eslint-parser'),
       parserOptions: {
-        parser: require("@babel/eslint-parser"),
+        parser: require('@babel/eslint-parser'),
         ecmaVersion: 2021,
-        sourceType: "module"
-      }
+        sourceType: 'module',
+      },
     },
     rules: {
-      "vue/no-unused-vars": "warn",
-      "prettier/prettier": ["error", { "endOfLine": "auto" }]
-    }
+      'vue/no-unused-vars': 'warn',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
   },
   ...compat.extends('plugin:vue/vue3-recommended'),
-  ...compat.extends('plugin:prettier/recommended')
+  ...compat.extends('plugin:prettier/recommended'),
 ];
+
