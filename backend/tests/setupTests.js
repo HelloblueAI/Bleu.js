481200abe165dc346c8bd0984555e5db14f49b6f
Pejman Haghighatnia <pejmanhnia@gmail.com>
2024-06-20 06:24:27 -0700

    Update CI/CD pipeline and fix linting and formatting issues

--- backend/tests/setupTests.js
+++ backend/tests/setupTests.js
@@ -1,35 +1,11 @@
 /* eslint-env node, jest */
-const axios = require('axios');
-const dotenv = require('dotenv');
-const jestExtended = require('jest-extended');
-const { faker } = require('@faker-js/faker');
-const MockDate = require('mockdate');
-
-dotenv.config();
-
-expect.extend(jestExtended);
-
-global.console = {
-  log: jest.fn(),
-  error: jest.fn(),
-  warn: jest.fn(),
-  info: jest.fn(),
-  debug: jest.fn(),
-};
-
-global.faker = faker;
-
-global.axios = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:8085',
-});
-
-MockDate.set('2024-01-01');
-
 beforeEach(() => {
   jest.clearAllMocks();
-});
-
-afterAll(() => {
-  jest.resetAllMocks();
-  MockDate.reset();
+  global.console = {
+    log: jest.fn(),
+    error: jest.fn(),
+    warn: jest.fn(),
+    info: jest.fn(),
+    debug: jest.fn(),
+  };
 });
