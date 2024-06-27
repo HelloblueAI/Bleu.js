5510c3cb897540de214171a2ac1bbd35979ff971
Pejman Haghighatnia <pejmanhnia@gmail.com>
2024-06-26 22:36:39 -0700

    Update CI/CD pipeline and configuration files

--- jest.setup.js
+++ jest.setup.js
@@ -1,40 +1,14 @@
-/* eslint-env jest */
-
-jest.setTimeout(30000);
-
-beforeAll(() => {
-  global.fetch = jest.fn(() =>
-    Promise.resolve({
-      json: () => Promise.resolve({}),
-    }),
-  );
-});
-
-afterAll(() => {
-  delete global.fetch;
-});
-
-beforeEach(() => {
-  global.console = {
-    log: jest.fn(),
-    error: jest.fn(),
-    warn: jest.fn(),
-    info: jest.fn(),
-    debug: jest.fn(),
-  };
-});
+import '@testing-library/jest-dom/extend-expect';
 
 afterEach(() => {
+  if (global.server) {
+    global.server.close(() => {
+      console.log('Server closed after test suite.');
+    });
+  }
+
   jest.restoreAllMocks();
   jest.resetModules();
   jest.clearAllMocks();
   jest.clearAllTimers();
-});
-
-process.on('unhandledRejection', (reason) => {
-  console.error('Unhandled Rejection:', reason);
-});
-
-process.on('uncaughtException', (error) => {
-  console.error('Uncaught Exception:', error);
 });
