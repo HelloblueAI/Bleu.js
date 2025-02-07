c449e83f17d6618ac8cc79456b2fd350f67dc313
Pejman Haghighatnia <pejmanhnia@gmail.com>
2025-02-06 18:19:20 -0800

    Update coverage

--- generateRuleId.js
+++ generateRuleId.js
@@ -1,8 +1,21 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const generateRuleId = () => {
-  console.log('Generating rule ID');
-  return uuidv4();
-};
+const generateRuleId = (() => {
+  const cache = new Set();
+
+  return () => {
+    let ruleId;
+    do {
+      ruleId = uuidv4();
+    } while (cache.has(ruleId));
+
+    cache.add(ruleId);
+
+    if (cache.size > 1000) cache.clear(); 
+
+    console.debug(`[RuleID Generator] New ID Generated: ${ruleId}`);
+    return ruleId;
+  };
+})();
 
 export default generateRuleId;
