358f72f3f9a21dec1a8322a197759646e9db45de
Pejman Haghighatnia <pejmanhnia@gmail.com>
2025-04-04 21:09:51 -0700

    chore: update .gitignore with comprehensive patterns

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'no-undef': 'error'
+  }
+}; 
