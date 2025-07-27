3529de0161e6c67c42b2aee43919a6461ffe0e73
Pejman Haghighatnia <pejmanhnia@gmail.com>
2025-07-27 01:32:58 -0700

    Clean up repository: Remove unused files and demo scripts

--- scripts/setup/test_tensorflow.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-print("TensorFlow version:", tf.__version__)
-print("Python executable:", tf.sysconfig.get_build_info()["executable"])
-print("Num GPUs Available:", len(tf.config.list_physical_devices("GPU")))
